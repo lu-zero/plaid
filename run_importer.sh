34455ead5bdf9f876488c5e394ead6d5ecec0ecb
Luca Barbato <lu_zero@gentoo.org>
2014-01-09 14:12:47 +0100

    tests: Update importer script
    Wipe the state before loading it.

--- run_importer.sh
+++ run_importer.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 # Setup a simple testcase
 
+rm -fR app.db migrations/
+
+python manage.py db init
+python manage.py db migrate
+python manage.py db upgrade
+
 python manage.py user -n admin -e admin@admin.com -p admin -r 1
 python manage.py project -n libav -l libav-devel -i libav-devel.libav.org
 python manage.py import --mailbox ./test/data/libav_1_99.mbox
