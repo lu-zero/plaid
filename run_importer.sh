45e63f9070d662988d5268d0e3a2f31ae0f8a88d
Federico Tomassetti <federico@tomassetti.me>
2015-08-14 13:01:50 +0200

    correct the test data used by run_importer.sh

--- run_importer.sh
+++ run_importer.sh
@@ -9,4 +9,4 @@ python manage.py db upgrade
 
 python manage.py user create -n admin -e admin@admin.com -p admin -r admin
 python manage.py project create -n libav -l libav-devel -i libav-devel.libav.org -d "Main development mailing list"
-python manage.py import --mailbox ./test/data/libav_ancestor.mbox
+python manage.py import --mailbox ./test/data/libav_1_99.mbox
