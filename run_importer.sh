a79a94f28bceaa1fdcec85ffa5ce16a31881dab1
Luca Barbato <lu_zero@gentoo.org>
2015-08-15 10:21:17 +0200

    test: Add the committer role

--- run_importer.sh
+++ run_importer.sh
@@ -8,5 +8,6 @@ python manage.py db migrate
 python manage.py db upgrade
 
 python manage.py user create -n admin -e admin@admin.com -p admin -r admin
+python manage.py user create -n committer -e committer -p committer -r committer
 python manage.py project create -n libav -l libav-devel -i libav-devel.libav.org -d "Main development mailing list"
 python manage.py import --mailbox ./test/data/libav_1_99.mbox
