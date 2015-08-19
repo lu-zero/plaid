08603a2f97ec6d500dde907d21ae04fff2b2c862
Federico Tomassetti <federico@tomassetti.me>
2015-08-19 15:42:44 +0200

    initial support for the admin interface

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, redirect
 from flask.ext.babel import Babel
 from flask.ext.mail import Mail
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.security import SQLAlchemyUserDatastore
-from flask.ext.security import Security
+from flask.ext.security import Security, current_user
 
 from config import configuration
 
@@ -24,6 +24,26 @@ from app import models
 from app import render
 from app import views
 
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from app.models import User, Patch, Role, Submitter, Project, Comment, Series, Topic, Tag
+
+
+class PlaidModelView(ModelView):
+
+    def is_accessible(self):
+        return current_user is not None and current_user.has_role('admin')
+
+admin = Admin(app, name='plaid')
+admin.add_view(PlaidModelView(User, db.session))
+admin.add_view(PlaidModelView(Role, db.session))
+admin.add_view(PlaidModelView(Patch, db.session))
+admin.add_view(PlaidModelView(Submitter, db.session))
+admin.add_view(PlaidModelView(Project, db.session))
+admin.add_view(PlaidModelView(Comment, db.session))
+admin.add_view(PlaidModelView(Series, db.session))
+admin.add_view(PlaidModelView(Topic, db.session))
+admin.add_view(PlaidModelView(Tag, db.session))
 
 user_datastore = SQLAlchemyUserDatastore(db, models.User, models.Role)
 security = Security(app, user_datastore,
